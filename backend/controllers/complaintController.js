08cede51716ffeb0638e9d3ba0767bb1769ad775
Jyothi Swaroop Reddy <bjyothiswaroop2002@gmail.com>
2022-11-07 00:40:16 +0530

    Added errorhandler for async functions

--- backend/controllers/complaintController.js
+++ backend/controllers/complaintController.js
@@ -1,27 +1,28 @@
 const Complaint = require("../models/complaintModel");
-const ErrorHandler = require("../utils/errorhandler")
+const ErrorHandler = require("../utils/errorhandler");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 
 // Create Complaint
-exports.createComplaint = async(req,res,next)=> {
+exports.createComplaint = catchAsyncErrors(async(req,res,next)=> {
     const complaint = await Complaint.create(req.body);
 
     res.status(201).json({
         success: true,
         complaint
     })
-}
+});
 
 // Get All Complaints
-exports.getAllComplaints = async(req,res) => {
+exports.getAllComplaints = catchAsyncErrors(async(req,res) => {
     const AllComplaints = await Complaint.find()
     res.status(200).json({
         success: true,
         AllComplaints
     });
-}
+});
 
 // Update Complaint
-exports.updateComplaint = async(req,res,next)=> {
+exports.updateComplaint = catchAsyncErrors(async(req,res,next)=> {
     let complaint1 = await Complaint.findById(req.params.id);
     if(!complaint1){
         return next(new ErrorHandler("Complaint not found",404));
@@ -33,19 +34,19 @@ exports.updateComplaint = async(req,res,next)=> {
         success: true,
         complaint1
     })
-}
+});
 
 // get User Complaints
-exports.getUserComplaints = async(req,res,next) => {
+exports.getUserComplaints = catchAsyncErrors(async(req,res,next) => {
     const complaints = await Complaint.find({user: req.params.id});
     res.status(201).json({
         success:true,
         complaints
     })
-}
+});
 
 // Delete User Complaint
-exports.deleteComplaint = async(req,res,next) => {
+exports.deleteComplaint = catchAsyncErrors(async(req,res,next) => {
     const complaint1 = await Complaint.findById(req.params.id);
     if(!complaint1) {
         return next(new ErrorHandler("Complaint not found",404));
@@ -55,4 +56,4 @@ exports.deleteComplaint = async(req,res,next) => {
         success: true,
         message: "Complaint Deletion successful"
     })
-}
+});
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,19 +1,19 @@
 const User = require("../models/UserModel");
-const ErrorHandler = require("../utils/errorhandler")
-
+const ErrorHandler = require("../utils/errorhandler");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 
 // Create User
-exports.createUser = async(req,res,next)=> {
+exports.createUser = catchAsyncErrors(async(req,res,next)=> {
     const user = await User.create(req.body);
 
     res.status(201).json({
         success: true,
         user
     })
-}
+});
 
 // Get Single User
-exports.getUser = async(req,res,next) => {
+exports.getUser = catchAsyncErrors(async(req,res,next) => {
     const UserData = await User.findById(req.params.id)
     if(!UserData) {
         return next(new ErrorHandler("User not found",404));
@@ -22,19 +22,19 @@ exports.getUser = async(req,res,next) => {
         success: true,
         UserData
     });
-}
+});
 
 // Get All users
-exports.getAllusers = async(req,res) => {
+exports.getAllusers = catchAsyncErrors(async(req,res) => {
     const UserData = await User.find()
     res.status(200).json({
         success: true,
         UserData
     });
-}
+})
 
 // Update User
-exports.updateUser = async(req,res,next)=> {
+exports.updateUser = catchAsyncErrors(async(req,res,next)=> {
     let user1 = await User.findById(req.params.id);
     if(!user1){
         return next(new ErrorHandler("User not found",404));
@@ -46,11 +46,11 @@ exports.updateUser = async(req,res,next)=> {
         success: true,
         user1
     })
-}
+});
 
 // Delete User
 
-exports.deleteUser = async(req,res,next) => {
+exports.deleteUser = catchAsyncErrors(async(req,res,next) => {
     const user1 = await User.findById(req.params.id);
     if(!user1) {
         return next(new ErrorHandler("User not found",404));
@@ -60,4 +60,4 @@ exports.deleteUser = async(req,res,next) => {
         success: true,
         message: "User Deletion successful"
     })
-}
+});
