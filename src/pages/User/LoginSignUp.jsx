dbe85695282df40736f0aaea2bb8831a3411aa48
Sai Krishna <saikrishnaa2002@gmail.com>
2022-11-09 20:09:12 +0530

    added login and register switching feature

--- src/pages/User/LoginSignUp.jsx
+++ src/pages/User/LoginSignUp.jsx
@@ -2,16 +2,25 @@ import React, { Fragment, useRef, useState, useEffect } from "react";
 import "./LoginSignUp.css";
 import Loader from "../layout/Loader/Loader";
 import { Link } from "react-router-dom";
-import MailOutlineIcon from "@material-ui/icons/MailOutline";
-import LockOpenIcon from "@material-ui/icons/LockOpen";
-import FaceIcon from "@material-ui/icons/Face";
 import { useDispatch, useSelector } from "react-redux";
 import { clearErrors, login, register } from "../../actions/userAction";
-import { useAlert } from "react-alert";
+import {
+  MDBBtn,
+  MDBContainer,
+  MDBCard,
+  MDBCardBody,
+  MDBCardImage,
+  MDBRow,
+  MDBCol,
+  MDBInput,
+  MDBCheckbox
+}
+  from 'mdb-react-ui-kit';
+
 
 const LoginSignUp = ({ history, location }) => {
   const dispatch = useDispatch();
-  const alert = useAlert();
+
 
   const { error, loading, isAuthenticated } = useSelector(
     (state) => state.user
@@ -58,14 +67,14 @@ const LoginSignUp = ({ history, location }) => {
 
   useEffect(() => {
     if (error) {
-      alert.error(error);
+
       dispatch(clearErrors());
     }
 
     if (isAuthenticated) {
       history.push(redirect);
     }
-  }, [dispatch, error, alert, history, isAuthenticated, redirect]);
+  }, [dispatch, error, history, isAuthenticated, redirect]);
 
   const switchTabs = (e, tab) => {
     if (tab === "login") {
@@ -90,38 +99,52 @@ const LoginSignUp = ({ history, location }) => {
         <Loader />
       ) : (
         <Fragment>
-          <div className="LoginSignUpContainer">
-            <div className="LoginSignUpBox">
-              <div>
+          <div style={{ marginTop: "100px", }}>
+            <div className="container">
+              <div >
                 <div className="login_signUp_toggle">
-                  <p onClick={(e) => switchTabs(e, "login")}>LOGIN</p>
-                  <p onClick={(e) => switchTabs(e, "register")}>REGISTER</p>
+                  <p onClick={(e) => switchTabs(e, "login")} className="LoginRegisterTitle">LOGIN</p>
+                  <p onClick={(e) => switchTabs(e, "register")} className="LoginRegisterTitle">REGISTER</p>
                 </div>
-                <button ref={switcherTab}></button>
+                <button className="switchButton" ref={switcherTab}></button>
               </div>
               <form className="loginForm" ref={loginTab} onSubmit={loginSubmit}>
-                <div className="loginFlatNo">
-                  <MailOutlineIcon />
-                  <input
-                    type="text"
-                    placeholder="FlatNo"
-                    required
-                    value={loginFlatNo}
-                    onChange={(e) => setLoginEmail(e.target.value)}
-                  />
-                </div>
-                <div className="loginPassword">
-                  <LockOpenIcon />
-                  <input
-                    type="password"
-                    placeholder="Password"
-                    required
-                    value={loginPassword}
-                    onChange={(e) => setLoginPassword(e.target.value)}
-                  />
-                </div>
-                <Link to="/password/forgot">Forget Password ?</Link>
-                <input type="submit" value="Login" className="loginBtn" />
+                <MDBContainer className='my-3'>
+                  <MDBCard className='p-5 login-container LoginContainer'>
+
+                    <MDBRow className='g-0 d-flex align-items-center'>
+
+                      <MDBCol md='4'>
+                        <MDBCardImage src='/src/assests/lodha11.webp' alt='logo' className='rounded-t-5 rounded-tr-lg-0' fluid />
+                      </MDBCol>
+
+                      <MDBCol md='8'>
+
+                        <MDBCardBody className="mx-auto">
+                          <div style={{ display: "flex", justifyContent: "center", alignItems: "center", marginBottom: "5%" }}>
+                            <img src="/src/assests/user.svg" style={{ width: "70px", height: "70px" }}></img>
+                            <h1 className='LoginHeading'>User Login</h1>
+                          </div>
+                          <div>
+                            <MDBInput wrapperClass='mb-4' placeholder='Email address' className='form1' id='uname' type='email' />
+                          </div>
+                          <MDBInput wrapperClass='mb-4' placeholder='Password' className='form1' id='pass' type='password' />
+
+                          <div className="d-flex justify-content-around mx-4 mb-4 form1">
+                            <MDBCheckbox name='flexCheck' value='' label='Remember me' />
+                            <Link to="/password/forgot">Forget Password ?</Link>
+                          </div>
+
+                          <MDBBtn className="mb-4 form1" id="loginButton">Login</MDBBtn>
+
+                        </MDBCardBody>
+
+                      </MDBCol>
+
+                    </MDBRow>
+
+                  </MDBCard>
+                </MDBContainer>
               </form>
               <form
                 className="signUpForm"
@@ -129,41 +152,53 @@ const LoginSignUp = ({ history, location }) => {
                 encType="multipart/form-data"
                 onSubmit={registerSubmit}
               >
-                <div className="signUpName">
-                  <FaceIcon />
-                  <input
-                    type="text"
-                    placeholder="Name"
-                    required
-                    name="name"
-                    value={name}
-                    onChange={registerDataChange}
-                  />
+                <div>
+                <MDBContainer className='my-3' style={{marginTop:"100px !important"}}>
+                  <MDBCard className='p-5 login-container registerContainer' >
+
+                    <MDBRow className='g-0 d-flex align-items-center'>
+
+                      <MDBCol md='4'>
+                        <MDBCardImage src='/src/assests/lodha11.webp' alt='logo' className='rounded-t-5 rounded-tr-lg-0' fluid />
+                      </MDBCol>
+
+                      <MDBCol md='8'>
+
+                        <MDBCardBody className="mx-auto">
+                          <div style={{ display: "flex", justifyContent: "center", alignItems: "center", marginBottom: "5%" }}>
+                            <img src="/src/assests/user.svg" style={{ width: "70px", height: "70px" }}></img>
+                            <h1 className='RegisterHeading'>Register</h1>
+                          </div>
+                          <MDBInput wrapperClass='mb-4' placeholder='Name' className='form1' id='name' type='text' />
+                          <div>
+                            <MDBInput wrapperClass='mb-4' placeholder='Email address' className='form1' id='RegUname' type='email' />
+                          </div>
+
+
+
+                          <MDBInput wrapperClass='mb-4' placeholder='Password' className='form1' id='RegPass' type='password' />
+                          <MDBInput wrapperClass='mb-4' placeholder='Mobile Number' className='form1' id='mob' type='text' />
+                          <MDBInput wrapperClass='mb-4' placeholder='Block' className='form1' id='block' type='text' />
+                          <MDBInput wrapperClass='mb-4' placeholder='Flat Number' className='form1' id='flat' type='number' />
+                          <MDBInput wrapperClass='mb-4' placeholder='Parking Slot' className='form1' id='parking' type='text' />
+                          <div className="d-flex justify-content-around mx-4 mb-4 form1">
+                            <MDBCheckbox name='flexCheck' value='' label='Remember me' />
+                            <a href="!#">Forgot password?</a>
+                          </div>
+
+                          <MDBBtn className="mb-4 form1" id="RegisterButton">Register</MDBBtn>
+
+                        </MDBCardBody>
+
+                      </MDBCol>
+
+                    </MDBRow>
+
+                  </MDBCard>
+                </MDBContainer>
                 </div>
-                <div className="signUpEmail">
-                  <MailOutlineIcon />
-                  <input
-                    type="email"
-                    placeholder="Email"
-                    required
-                    name="email"
-                    value={email}
-                    onChange={registerDataChange}
-                  />
-                </div>
-                <div className="signUpPassword">
-                  <LockOpenIcon />
-                  <input
-                    type="password"
-                    placeholder="Password"
-                    required
-                    name="password"
-                    value={password}
-                    onChange={registerDataChange}
-                  />
-                </div>
-                <input type="submit" value="Register" className="signUpBtn" />
               </form>
+
             </div>
           </div>
         </Fragment>
